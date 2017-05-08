64e2643c956f5a0a034640bd4ba6ee4a04468f09
SEDGWICK <alexandersedgwick@northwesternmutual.com>
2017-05-08 11:11:46 -0500

    Added project file

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject csv-tools "0.1.0-SNAPSHOT"
+  :description "Extending clojure.csv"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "csv-tools")
