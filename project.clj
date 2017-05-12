29ecaa05fc9eeefa006564de7ebcc68f41f74d2c
SEDGWICK <alexandersedgwick@northwesternmutual.com>
2017-05-12 09:05:30 -0500

    Simplified Project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csv-tools "0.1.0-SNAPSHOT"
+(defproject zaex.csv-tools "0.1.0-SNAPSHOT"
   :description "Extending clojure.csv"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :min-lein-version "2.0.0"
